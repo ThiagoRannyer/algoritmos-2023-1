cc8c1cca581156b1b1bc053d2f3b878ecc753eb8
ThiagoRannyer <65875751+ThiagoRannyer@users.noreply.github.com>
2023-05-15 17:09:15 -0300

    Add files via upload

---
+++ src/2023-03-31/exercicio1/p3.py
@@ -0,0 +1,9 @@
+'''
+Faça um Programa que peça dois números e imprima a soma.
+'''
+numero1 = int(input('Digite um numero:'))
+numero2 = int(input('Digite outro numero:'))
+
+soma = numero1+numero2
+
+print(f'A soma dos dois números é: {soma}')
---
+++ src/2023-03-31/exercicio1/p5.py
@@ -0,0 +1,9 @@
+'''
+Faça um Programa que converta metros para centímetros.
+
+'''
+
+metro = int(input('Digite um numero em metros: '))
+resultado = metro*100
+
+print(f'{metro} metro(s) convertido em centimetros é: {resultado}cm ')
